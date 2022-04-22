b4219b44b8dae58e98b44288b9bbedfb98d4db44
Bastán loyola <bastian.loyola@usach.cl>
2022-04-22 14:54:37 -0400

    [Bastian Loyola] - Creacion del modelo tarea y tablas intermedias

---
+++ backend-ayni/src/main/java/cl/tbd/backendayni/models/EstadoTarea.java
@@ -0,0 +1,23 @@
+package cl.tbd.backendayni.models;
+
+public class EstadoTarea {
+    private Estado estado;
+    private Tarea tarea;
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+	public Tarea getTarea() {
+		return tarea;
+	}
+
+	public void setTarea(Tarea tarea) {
+		this.tarea = tarea;
+	}
+    
+}

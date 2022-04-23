0fc3ba0fde34b9c103d70f394c8c6c350f3ea65f
CarlaP <carla.polanco@usach.cl>
2022-04-22 22:37:05 -0400

    [Carla Polanco] - View Tareas
    Posee un error de diseño se se corregirá posteriormente.

--- frontend-ayni/components/Navbar.vue
+++ frontend-ayni/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #169691">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="/"><img src = "../store/ayni-icon.png" height = 50 ></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -9,6 +9,9 @@
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./Tareas">Tareas</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Quienes Somos</a>
---
+++ frontend-ayni/components/Tareas.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wrapper fadeInDown scrollDiv" style="background-color: #4F3185">
+  
+   <div class="contenedor">  
+
+    <div>
+        <h1> Emergencia: blablabla</h1>
+        <a href="#">
+        <button class="boton"><span>Tarea1</span></button>
+        </a>
+    </div>
+
+    <div>
+        <h1> Emergencia: blablabla</h1>
+        <a href="#">
+        <button class="boton"><span>Tarea2</span></button>
+        </a>
+    </div>
+
+    <div>
+        <h1> Emergencia: blablabla</h1>
+        <a href="#">
+        <button class="boton"><span>Tarea3</span></button>
+        </a>
+    </div>
+    
+  </div>
+
+
+  </div>
+  </div>
+
+</template>
+
+<style>
+.scrollDiv {
+    height: 100vh;
+    max-height: 100vh;
+}
+</style>

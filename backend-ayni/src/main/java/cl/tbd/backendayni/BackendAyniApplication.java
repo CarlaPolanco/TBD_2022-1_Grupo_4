d22ad5c01483da2dce75aa39e16cdeca5d3cad4b
Anri <estefania.alvarez@usach.cl>
2022-04-23 17:45:51 -0400

    [Estefanía Alvarez] - Creación carpetas services y repositories
    También agregado el DatabaseContext

--- backend-ayni/src/main/java/cl/tbd/backendayni/BackendAyniApplication.java
+++ backend-ayni/src/main/java/cl/tbd/backendayni/BackendAyniApplication.java
@@ -8,6 +8,16 @@ public class BackendAyniApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendAyniApplication.class, args);
+
+		Sql2o base = DatabaseContext.sql2o();
+		try{
+			Connection conn = base.open();
+			System.out.println("si funciona");
+
+        }
+		catch(Sql2oException e){
+			System.out.println("no funciona");
+		}
 	}
 
 }

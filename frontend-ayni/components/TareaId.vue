9bd5d22c1f4e00795c993d59f2b18f4862dec7d3
Bastian Onetto <Bastian.onetto@usach.cl>
2022-05-01 12:15:09 -0400

    [Bastian Onetto] - Creacion y bosquejo pagina seleccion Voluntario

--- frontend-ayni/components/TareaId.vue
+++ frontend-ayni/components/TareaId.vue
@@ -25,7 +25,7 @@
 
       <div class="contenedorDos">
         <div>
-          <a href="#">
+          <a href="./SelectVolTarea">
             <button type="button" class="btn btn-primary btn-lg">
               Aceptar
             </button>
---
+++ frontend-ayni/pages/SelectVolTarea.vue
@@ -0,0 +1,6 @@
+<template>
+    <body>
+        <Navbar/>
+        <TareaListaVoluntario/>
+    </body>
+</template>
